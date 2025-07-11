07ce1306941a04716c4984fef2a494e740a11a58
psjain13 <psjain@wpi.edu>
2025-07-11 16:27:43 +0400

    Added Search Bar to Projects. Sorted Projects into various categories. Added a placeholder card for new projects.

--- script.js
+++ script.js
@@ -20,3 +20,16 @@ function toggleMenu() {
   const menu = document.getElementById('nav-menu');
   menu.classList.toggle('show');
 }
+
+// Projects Search Bar
+function filterAllProjects(value) {
+  const cards = document.querySelectorAll(".projects-grid .card");
+  value = value.toLowerCase();
+  cards.forEach(card => {
+    const title = card.querySelector("h3").textContent.toLowerCase();
+    const tags = Array.from(card.querySelectorAll(".tags span")).map(tag => tag.textContent.toLowerCase());
+    const match = title.includes(value) || tags.some(tag => tag.includes(value));
+    card.style.display = match ? "block" : "none";
+  });
+}
+
